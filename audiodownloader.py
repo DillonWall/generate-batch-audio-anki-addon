572d8156ae794d062d670ff9862167c2c3f1af47
DillonWall <dillonmwall@msn.com>
2022-11-22 15:10:34 -0800

    Init commit v1.0

---
+++ audiodownloader.py
@@ -0,0 +1,206 @@
+"""
+This addon and all code included is open-source under the Apache-2.0 License
+
+CREDIT TO Yomichan by FooSoft Productions for the idea of hashing the audio files to be able to compare them.
+https://foosoft.net/projects/yomichan/
+
+Author:         Dillon Wall
+Description:    This file handles all API requests and audio specific functions.
+                The main functionality of this file is to use an AudioDownloader object to download the audio file
+                    for an Anki card, save it to the user's collection.media, and return the file name.
+"""
+import requests
+import json
+import re
+import os
+import hashlib
+import mimetypes
+
+DEFAULT_TIMEOUT = 15
+
+
+# Static funcs
+def substitute_string_vars(s, replace_dict, duplicate_fld):
+    """
+    Substitutes any variable in {} in the string (s) with a corresponding entry in replace_dict
+    """
+    # validate string so that all variable names (between {}) are lowercase
+    val_str = ""
+    for sub_str in re.split('({[^}]*})', s):
+        if sub_str.startswith('{'):
+            val_str += sub_str.lower()
+        else:
+            val_str += sub_str
+
+    relevant_keys = []
+    prev_val = ""
+    for key, value in replace_dict.items():
+        if val_str.find("{" + key.lower() + "}") >= 0:
+            if value == "" and duplicate_fld:
+                value = prev_val
+            relevant_keys.append(key)
+            val_str = val_str.replace("{" + key.lower() + "}", value)
+            prev_val = value
+
+    return val_str, relevant_keys
+
+
+def get_request(url, allow_redirects=True):
+    """
+    Performs a GET request to the specified url
+    Uses a predefined User-Agent to make it look like we are accessing it from the browser
+    """
+    if url is None or not url.startswith("http"):
+        raise Exception(url)
+
+    # In order to bypass certain limitations from sites that require a browser to access,
+    #   this header makes it look like we are using a browser instead of whatever is default
+    response = requests.request(
+        method="GET",
+        headers={
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'},
+        url=url,
+        timeout=DEFAULT_TIMEOUT
+    )
+
+    if not response:
+        raise IOError("No response for %s", url)
+
+    if response.status_code != 200:
+        value_error = ValueError(
+            "Got %d status for %s" %
+            (response.status_code, url)
+        )
+        try:
+            value_error.payload = response.content
+            response.close()
+        except Exception:
+            pass
+        raise value_error
+
+    if not allow_redirects and response.geturl() != url:
+        raise ValueError("Request has been redirected")
+
+    content_type = response.headers.get('Content-Type', '')
+    is_json = 'application/json' in content_type
+    audio_ext = None
+    if not is_json:
+        audio_ext = mimetypes.guess_extension(content_type)
+        if not audio_ext:
+            if 'mpeg' in content_type:
+                audio_ext = '.mp3'
+            elif 'aac' in content_type:
+                audio_ext = '.aac'
+    payload = response.content
+    response.close()
+
+    return payload, is_json, audio_ext
+
+
+def filter_urls_list(arr):
+    """
+    Recursive helper for  filter_urls_dict
+    """
+    for val in arr:
+        if type(val) is dict:
+            return filter_urls_dict(val)
+        elif type(val) is list:
+            return filter_urls_list(val)
+        else:
+            return  # we shouldn't get here?
+
+
+def filter_urls_dict(dictionary):
+    """
+    Expects a dictionary
+    Recursively searches and filters out any urls from the json
+    for now, this just returns the first url it finds
+    """
+    # raise Exception(dictionary)
+    for key in dictionary:
+        if type(dictionary[key]) is dict:
+            return filter_urls_dict(dictionary[key])
+        elif type(dictionary[key]) is list:
+            return filter_urls_list(dictionary[key])
+        elif key.lower() == 'url':
+            return dictionary[key]
+
+
+def save_from_https(media_content, output_file_name):
+    """
+    Saves a file to the user's media collection
+    """
+    open(output_file_name, 'wb').write(media_content)
+
+
+def create_file_name(args_dict, relevant_keys, audio_name, audio_ext):
+    """
+    Create a new file name from the provided args dict
+    """
+    filename = audio_name
+    for key in relevant_keys:
+        if key in args_dict:
+            filename += '_' + str(args_dict[key])
+
+    filename = filename.replace('[', '').replace(']', '')    # remove brackets
+
+    return filename + audio_ext
+
+
+def shaHashDigest(hashItem):
+    """
+    # Credit Yomichan for the idea, refactored the audio hashing system for use in python
+    Hashes the given hashItem into SHA-256 format and returns the hex digest
+    """
+    m = hashlib.sha256()
+    m.update(hashItem)
+
+    digest = m.hexdigest()
+
+    return digest
+
+
+def isAudioBinaryValid(hashItem):
+    """
+    # Credit Yomichan for the idea, refactored the audio hashing system for use in python
+    Checks the audio binary and makes sure it isn't the JPod101 no-audio audio file
+    """
+    digest = shaHashDigest(hashItem)
+    if digest == 'ae6398b5a27bc8c0a771df6c907ade794be15518174773c58c7c7ddd17098906':    # Invalid audio file hash
+        return False
+    return True
+
+
+class AudioDownloader:
+    def __init__(self, audio_sources, mw):
+        self.audio_sources = audio_sources
+        self.mw = mw
+
+    def download_single(self, args_dict, duplicate_fld):
+        """
+        Downloads an audio file based on the args_dict,
+            saves it to the user's collection.media,
+            and returns the file name.
+        """
+        # try each in order, if result is nothing or error occurs try next
+        for audio_name, raw_url in self.audio_sources.items():
+            try:
+                get_url, relevant_keys = substitute_string_vars(raw_url, args_dict, duplicate_fld)
+                request_payload, is_json, audio_ext = get_request(get_url)
+                if is_json:
+                    json_dict_payload = json.loads(request_payload)
+                    download_url = filter_urls_dict(json_dict_payload)
+                    request_payload, _, audio_ext = get_request(download_url)
+
+                if not isAudioBinaryValid(request_payload):
+                    continue
+
+                output_file_name = create_file_name(args_dict, relevant_keys, audio_name, audio_ext)
+                full_output_file_name = os.path.join(self.mw.col.media.dir(), output_file_name)
+                save_from_https(request_payload, full_output_file_name)
+
+                return output_file_name
+            except Exception:
+                pass
+
+        return None
---
+++ main.py
@@ -0,0 +1,470 @@
+"""
+This addon and all code included is open-source under the Apache-2.0 License
+
+CREDIT TO Batch Editing ADDON https://ankiweb.net/shared/info/291119185
+Most of the code structure is based on ^this^ addon
+
+Author:         Dillon Wall
+Description:    An addon for Anki that downloads and adds audio in a bulk operation to the user-selected cards.
+                This addon makes use of URLs to submit API requests and download audio from any URL the user specifies
+                    in an order of priority.
+"""
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+
+import time
+
+from aqt.qt import *
+from aqt.utils import tooltip
+
+from anki.hooks import addHook
+from aqt import mw
+from aqt.operations import QueryOp
+
+from .audiodownloader import *
+
+# Config setup
+config = mw.addonManager.getConfig(__name__)
+
+
+def deleteItemsOfLayout(layout):
+    """
+    Helper function that recursively deletes all items within a layout
+    """
+    if layout is not None:
+        while layout.count():
+            item = layout.takeAt(0)
+            widget = item.widget()
+            if widget is not None:
+                widget.setParent(None)
+            else:
+                deleteItemsOfLayout(item.layout())
+
+
+class SourceHBox(QHBoxLayout):
+    def __init__(self, priority, changePriority, removeSource, parentDialog):
+        QHBoxLayout.__init__(self)
+
+        self.priority = priority
+        self.changePriority = changePriority
+        self.removeSource = removeSource
+        self.parentDialog = parentDialog
+
+        self.position_label = QLabel(str(self.priority) + ": ")
+
+        self.name_textbox = QLineEdit()
+        self.name_textbox.setPlaceholderText("Enter Name")
+        self.name_textbox.setMaximumWidth(80)
+
+        self.cu_textbox = QLineEdit()
+        self.cu_textbox.setPlaceholderText("Enter Custom URL")
+
+        self.source_button_box = QDialogButtonBox(Qt.Horizontal, self.parentDialog)
+        self.source_button_box.setMaximumWidth(60)
+        self.moveup_btn = self.source_button_box.addButton("^",
+                                        QDialogButtonBox.ActionRole)
+        self.moveup_btn.setToolTip("Move this source up in priority")
+        self.moveup_btn.clicked.connect(lambda state, dir="up": self.changePriorityHelper(dir))
+        self.movedown_btn = self.source_button_box.addButton("v",
+                                        QDialogButtonBox.ActionRole)
+        self.movedown_btn.setToolTip("Move this source down in priority")
+        self.movedown_btn.clicked.connect(lambda state, dir="down": self.changePriorityHelper(dir))
+        self.remove_btn = self.source_button_box.addButton("X",
+                                        QDialogButtonBox.ActionRole)
+        self.remove_btn.setToolTip("Remove this source")
+        self.remove_btn.clicked.connect(lambda state: self.removeSourceHelper())
+
+        self.addWidget(self.position_label)
+        self.addWidget(self.name_textbox)
+        self.addWidget(self.cu_textbox)
+        self.addWidget(self.source_button_box)
+
+    def changePriorityNumber(self, amount):
+        self.priority += amount
+        self.setPriorityNumber(self.priority)
+
+    def setPriorityNumber(self, amount):
+        self.priority = amount
+        self.position_label.setText(str(self.priority) + ": ")
+        self.position_label.repaint()
+
+    def changePriorityHelper(self, dir):
+        self.changePriority(dir, self.priority)
+
+    def removeSourceHelper(self):
+        self.removeSource(self.priority)
+
+    def getInfo(self):
+        return self.name_textbox.text(), self.cu_textbox.text()
+
+    def loadFromDict(self, dict):
+        self.setPriorityNumber(dict['priority'])
+        self.name_textbox.setText(dict['name'])
+        self.cu_textbox.setText(dict['url'])
+
+    def saveAsDict(self):
+        dict = {
+            'priority': self.priority,
+            'name': self.name_textbox.text(),
+            'url': self.cu_textbox.text()
+        }
+        return dict
+
+
+class BulkGenerateDialog(QDialog):
+    """Browser batch editing main dialog"""
+
+    def __init__(self, browser, nids):
+        QDialog.__init__(self, parent=browser)
+        self.browser = browser
+        self.nids = nids
+        self.num_sources = 0
+        self.sources = []
+        self.sources_vbox = QVBoxLayout()
+
+        self._setupUi()
+
+    def saveSourcesToConfig(self):
+        """
+        Saves just the sources to the config
+        (I should get rid of this function and put the functionality in saveConfig since we really only need to call
+            saveConfig when the window closes)
+        """
+        sourcesDict = []
+        for s in self.sources:
+            sourcesDict.append(s.saveAsDict())
+
+        config['sources'] = sourcesDict
+        mw.addonManager.writeConfig(__name__, config)
+
+    def saveConfig(self):
+        """
+        Saves all UI elements to the config
+        """
+        self.saveSourcesToConfig()
+        config['fsel'] = self.fsel.currentText()
+        config['delay_sb'] = self.delay_sb.value()
+        config['ksel'] = self.ksel.currentText()
+        config['dup_cb'] = self.dup_cb.isChecked()
+        config['danger_cb'] = self.danger_cb.isChecked()
+        mw.addonManager.writeConfig(__name__, config)
+
+    def loadConfig(self):
+        """
+        Loads all UI elements from the config
+        """
+        if config['sources'] and len(config['sources']) > 0:
+            for i, s in enumerate(config['sources']):
+                self.addSource()
+                self.sources[i].loadFromDict(s)
+        else:
+            self.addSource()
+            self.saveSourcesToConfig()
+
+        if config['fsel'] and self.fsel.findText(config['fsel']):
+            self.fsel.setCurrentText(config['fsel'])
+        if config['delay_sb']:
+            self.delay_sb.setValue(config['delay_sb'])
+        if config['ksel'] and self.ksel.findText(config['ksel']):
+            self.ksel.setCurrentText(config['ksel'])
+        if config['dup_cb']:
+            self.dup_cb.setChecked(config['dup_cb'])
+        if config['danger_cb']:
+            self.danger_cb.setChecked(config['danger_cb'])
+
+    def closeEvent(self, evnt):
+        """
+        Overrides the closeEvent of the main window
+        Before closing, it saves all UI elements to the config
+        """
+        self.saveConfig()
+        super(BulkGenerateDialog, self).closeEvent(evnt)
+
+    def _setupUi(self):
+        """
+        Sets up all the UI elements in the main BulkGenerateDialog window
+        """
+        # Target audio field, Filter kana field, Delay spin box, and Duplicate check box
+        flabel = QLabel("Audio field:")
+        flabel.setToolTip("This specifies the field that will get REPLACED with the downloaded audio clip on every selected card")
+        self.fsel = QComboBox()
+        fields = self._getFields()
+        self.fsel.addItems(fields)
+
+        dlabel = QLabel("Delay between requests:")
+        dlabel.setToolTip("This specifies the delay in seconds between each HTTP request to download audio.\n\nIf you are downloading from Forvo or another source that has bot detection, use a VPN or set this to a higher delay.")
+        self.delay_sb = QDoubleSpinBox()
+        self.delay_sb.setDecimals(1)
+        self.delay_sb.setMinimum(0)
+        self.delay_sb.setMaximum(999)
+        self.delay_sb.setValue(0)
+
+        klabel = QLabel("Filter kana:")
+        klabel.setToolTip("This specifies a field to filter out all characters except hiragana.\n\nThis is useful for the 'Reading' field if you have it in the form 私[わたし] instead of わたし.")
+        self.ksel = QComboBox()
+        fields = self._getFields()
+        self.ksel.addItem("(None)")
+        self.ksel.addItems(fields)
+
+        duplabel = QLabel("Duplicate to empty fields:")
+        duplabel.setToolTip("If enabled, will duplicate the previously used URL parameter to the next parameter if empty.")
+        self.dup_cb = QCheckBox()
+        self.dup_cb.setChecked(True)
+
+        dangerlabel = QLabel("Dangerously fast:")
+        dangerlabel.setToolTip("If enabled, will run 0.1s faster per card, but likely give you errors.")
+        self.danger_cb = QCheckBox()
+
+        f_grid = QGridLayout()
+        f_grid.addWidget(flabel, 0, 1)
+        f_grid.addWidget(self.fsel, 0, 2)
+        f_grid.setColumnStretch(3, 1)
+        f_grid.addWidget(duplabel, 0, 4, 1, 3)
+        f_grid.addWidget(self.dup_cb, 0, 7)
+        f_grid.setColumnStretch(8, 2)
+
+        f_grid.addWidget(klabel, 1, 1)
+        f_grid.addWidget(self.ksel, 1, 2)
+        f_grid.addWidget(dangerlabel, 1, 4, 1, 3)
+        f_grid.addWidget(self.danger_cb, 1, 7)
+
+        f_grid.addWidget(dlabel, 2, 1)
+        f_grid.addWidget(self.delay_sb, 2, 2)
+
+        # setup buttons
+        button_box_hbox = QHBoxLayout()
+        # button box LEFT
+        button_box_vbox_left = QVBoxLayout()
+        button_box_left = QDialogButtonBox(Qt.Horizontal, self)
+
+        addsource_btn = button_box_left.addButton("Add source",
+                                        QDialogButtonBox.ActionRole)
+        addsource_btn.setToolTip("Add a new source")
+        addsource_btn.clicked.connect(lambda state: self.addSource())
+
+        button_box_vbox_left.addWidget(button_box_left, alignment=Qt.AlignLeft)
+
+        # button box RIGHT
+        button_box_vbox_right = QVBoxLayout()
+        button_box_right = QDialogButtonBox(Qt.Horizontal, self)
+
+        generate_btn = button_box_right.addButton("Generate",
+                                        QDialogButtonBox.ActionRole)
+        generate_btn.setToolTip("Generate audio for all selected cards")
+        generate_btn.clicked.connect(lambda state: self.onGenerate())
+        button_box_vbox_right.addWidget(button_box_right)
+
+        # bottom controls layout
+        self.layout_bottom = QVBoxLayout()
+        self.layout_bottom.addLayout(f_grid)
+        self.layout_bottom.addSpacing(20)
+        button_box_hbox.addLayout(button_box_vbox_left)
+        button_box_hbox.addLayout(button_box_vbox_right)
+        self.layout_bottom.addLayout(button_box_hbox)
+
+        # main vbox layout
+        self.vbox_main = QVBoxLayout()
+        self.vbox_main.addLayout(self.sources_vbox)
+        self.vbox_main.addLayout(self.layout_bottom)
+
+        # alignment
+        self.layout_bottom.setAlignment(Qt.AlignBottom)
+        button_box_vbox_left.setAlignment(Qt.AlignLeft)
+        button_box_vbox_right.setAlignment(Qt.AlignRight)
+
+        self.setLayout(self.vbox_main)
+        generate_btn.setFocus()
+        self.setMinimumWidth(540)
+        self.setMinimumHeight(400)
+        self.setWindowTitle("Bulk Generate Audio for Selected Notes")
+
+        self.loadConfig()
+
+    def _changePriority(self, dir, source):
+        """
+        Changes the priority of a source in the list
+        """
+        if dir == 'up' and source > 1:
+            # modify this one and the above one
+            self.sources[source - 1].changePriorityNumber(-1)
+            self.sources[source - 2].changePriorityNumber(1)
+
+            temp = self.sources[source - 1]
+            self.sources[source - 1] = self.sources[source - 2]
+            self.sources[source - 2] = temp
+
+            for source_id in range(source - 1, self.num_sources + 1):
+                self.sources_vbox.removeItem(self.sources[source_id - 1])
+                self.sources_vbox.insertLayout(source_id - 1, self.sources[source_id - 1])
+        elif dir == 'down' and source < self.num_sources:
+            # modify this one and the below one
+            self.sources[source].changePriorityNumber(-1)
+            self.sources[source - 1].changePriorityNumber(1)
+
+            temp = self.sources[source]
+            self.sources[source] = self.sources[source - 1]
+            self.sources[source - 1] = temp
+
+            for source_id in range(source, self.num_sources + 1):
+                self.sources_vbox.removeItem(self.sources[source_id - 1])
+                self.sources_vbox.insertLayout(source_id - 1, self.sources[source_id - 1])
+
+    def _removeSource(self, source):
+        """
+        Removes a source from the list of sources
+        """
+        self.num_sources -= 1
+        deleteItemsOfLayout(self.sources[source - 1])
+        self.sources_vbox.removeItem(self.sources[source - 1])
+        for source_id in range(source + 1, self.num_sources + 2):
+            self.sources[source_id - 1].changePriorityNumber(-1)
+            self.sources_vbox.removeItem(self.sources[source_id - 1])
+            self.sources_vbox.insertLayout(source_id-1, self.sources[source_id - 1])
+
+        del self.sources[source - 1]
+
+    def _getFields(self):
+        """
+        Returns all the fields on the first card selected
+        Note: That this means if multiple different card types are selected, there may be some issues/unsuccessful cards
+        """
+        nid = self.nids[0]
+        mw = self.browser.mw
+        model = mw.col.getNote(nid).model()
+        fields = mw.col.models.fieldNames(model)
+        return fields
+
+    def addSource(self):
+        """
+        Adds a new SourceHBox to the list of sources
+        """
+        self.num_sources += 1
+        source = SourceHBox(priority=self.num_sources, changePriority=self._changePriority, removeSource=self._removeSource,
+                             parentDialog=self)
+        self.sources.append(source)
+        self.sources_vbox.addLayout(source)
+
+    def onGenerate(self):
+        """
+        Actually generate the audio for each card selected
+        This is the on call function for the generate button
+        """
+        filter_kana_fld = self.ksel.currentText()
+        duplicate_fld = self.dup_cb.isChecked()
+        sleep_time = self.delay_sb.value() + (0.0 if self.danger_cb.isChecked() else 0.1)
+        audio_sources = {}
+        for source in self.sources:
+            name, url = source.getInfo()
+            audio_sources.update({name: url})
+
+        browser = self.browser
+        mw = browser.mw
+        nids = self.nids
+        audio_fld = self.fsel.currentText()
+        ad = AudioDownloader(audio_sources, mw)
+
+        mw.checkpoint("batch add audio")
+        mw.progress.start()
+        browser.model.beginReset()
+
+        op = QueryOp(
+            parent=mw,
+            op=lambda col: _processNotes(browser, nids, audio_fld, sleep_time, filter_kana_fld, ad, duplicate_fld),
+            success=_processNotesOnSuccess
+        )
+        op.with_progress().run_in_background()
+
+
+def _processNotes(browser, nids, audio_fld, sleep_time, filter_kana_fld, ad, duplicate_fld):
+    """
+    The main function that handles downloading and adding audio to each card
+    This function is meant to run in the background as to not hang the main window
+    """
+    total = len(nids)
+    cnt = 0
+    last_progress = 0
+    for nid in nids:
+        # Update progress
+        if time.time() - last_progress >= 0.1:
+            mw.taskman.run_on_main(
+                lambda: mw.progress.update(
+                    label=f"{cnt} / {total} cards updated",
+                    value=cnt,
+                    max=total,
+                )
+            )
+            last_progress = time.time()
+
+        note = mw.col.getNote(nid)
+
+        if audio_fld in note:
+            # IMPORTANT: BEFORE DOWNLOADING, SLEEP FOR SET TIME IF APPLICABLE
+            if sleep_time > 0:
+                time.sleep(sleep_time)
+
+            note_dict = {}
+            for key, value in note.items():
+                if key == filter_kana_fld:  # If '(None)' then it shouldn't find it anyway (unless someone has that as their field name for some reason)
+                    value = filter_kana(value)
+                note_dict.update({key: value})
+
+            audio_filename = ad.download_single(note_dict, duplicate_fld)
+            if audio_filename:
+                note[audio_fld] = '[sound:' + audio_filename + ']'
+                note.flush()
+            cnt += 1
+
+        if mw.progress.want_cancel():
+            break
+
+    return browser, cnt
+
+
+def _processNotesOnSuccess(browser_cnt_tuple):
+    """
+    On success function for the _processNotes function
+    Resets necessary items and displays a tooltip of the amount of notes updated
+    """
+    browser = browser_cnt_tuple[0]
+    cnt = browser_cnt_tuple[1]
+    browser.model.endReset()
+    mw.requireReset()
+    mw.progress.finish()
+    mw.reset()
+    tooltip("<b>Updated</b> {0} notes.".format(cnt), parent=browser)
+
+
+def filter_kana(string):
+    """
+    Use a Regex filter to only return the hiragana from a string
+    """
+    hiragana_full = r'[ぁ-ゟ]'
+    return ''.join(re.findall(hiragana_full, string))
+
+
+def onBulkGenerate(browser):
+    """
+    CREDIT TO Batch Editing ADDON https://ankiweb.net/shared/info/291119185
+    Modified slightly to work for this addon instead
+    """
+    nids = browser.selectedNotes()
+    if not nids:
+        tooltip("No cards selected.")
+        return
+    dialog = BulkGenerateDialog(browser, nids)
+    dialog.exec_()
+
+
+def setupMenu(browser):
+    """
+    CREDIT TO Batch Editing ADDON https://ankiweb.net/shared/info/291119185
+    Modified slightly to work for this addon instead
+    """
+    menu = browser.form.menuEdit
+    menu.addSeparator()
+    a = menu.addAction('Generate Bulk Audio...')
+    a.setShortcut(QKeySequence("Ctrl+Alt+B"))
+    a.triggered.connect(lambda _, b=browser: onBulkGenerate(b))
+
+
+addHook("browser.setupMenus", setupMenu)
